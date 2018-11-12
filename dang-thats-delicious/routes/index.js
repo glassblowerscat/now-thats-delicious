c413c39f61810f4b6b4961b0d6defb15ed078c89
glassblowerscat <glassblower.random@gmail.com>
2018-11-11 22:32:39 -0500

    Created registration, login, and authentication flows and forms

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
@@ -20,5 +22,17 @@ router.get('/stores/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags/', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+// 1. Validate the reigstration data
+// 2. Register the user
+// 3. Log them in
+router.post('/register',
+  userController.validateRegister,
+  userController.register,
+  authController.login
+);
 
 module.exports = router;
