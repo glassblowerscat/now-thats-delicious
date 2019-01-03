01c8a9b8debe9ddc977391e7b04ec220f1f860e3
Ryan Elainska <ryan@elainska.com>
2019-01-03 09:08:20 -0500

    Changed env variables, created a couple stores, adding .gitignore to repo

--- data/load-sample-data.js
+++ data/load-sample-data.js
@@ -1,8 +1,10 @@
-require('dotenv').config({ path: __dirname + '/../variables.env' });
+require('dotenv').config({
+  path: __dirname + '/../variables.env'
+});
 const fs = require('fs');
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.MONGODB_URI);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 
 // import all of our models - they need to be imported only once
@@ -31,7 +33,7 @@ async function loadData() {
     await User.insertMany(users);
     console.log('👍👍👍👍👍👍👍👍 Done!');
     process.exit();
-  } catch(e) {
+  } catch (e) {
     console.log('\n👎👎👎👎👎👎👎👎 Error! The Error info is below but if you are importing sample data make sure to drop the existing database first with.\n\n\t npm run blowitallaway\n\n\n');
     console.log(e);
     process.exit();
--- start.js
+++ start.js
@@ -8,10 +8,12 @@ if (major < 7 || (major === 7 && minor <= 5)) {
 }
 
 // import environmental variables from our variables.env file
-require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config({
+  path: 'variables.env'
+});
 
 // Connect to our Database and handle any bad connections
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.MONGODB_URI);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
